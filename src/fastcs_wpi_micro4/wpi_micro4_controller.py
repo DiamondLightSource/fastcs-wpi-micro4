ac9ec0e9008a8b2198407e43de71c71593d2c04a
rqq82173 <urszula.neuman@diamond.ac.uk>
2025-11-21 12:40:46 +0000

    Cahnge line nummber with command

--- src/fastcs_wpi_micro4/wpi_micro4_controller.py
+++ src/fastcs_wpi_micro4/wpi_micro4_controller.py
@@ -1,19 +1,44 @@
+import asyncio
 from dataclasses import KW_ONLY, dataclass
 from typing import TypeVar
 
 from fastcs.attribute_io import AttributeIO
 from fastcs.attribute_io_ref import AttributeIORef
-from fastcs.attributes import AttrR, AttrRW, AttrW
+from fastcs.attributes import AttrR, AttrW
 from fastcs.connections import (
     IPConnection,
     IPConnectionSettings,
 )
 from fastcs.controller import Controller
-from fastcs.datatypes import Float, Int, String
+from fastcs.datatypes import Float, Int
+from fastcs.wrappers import command
 
 NumberT = TypeVar("NumberT", int, float)
 
 
+@dataclass
+class WpiMicro4ControllerSettingIORef(AttributeIORef):
+    name: str
+    _: KW_ONLY
+    update_period: float | None = None
+
+
+class WpiMicro4ControllerSettingIO(
+    AttributeIO[NumberT, WpiMicro4ControllerSettingIORef]
+):
+    def __init__(self, connection: IPConnection):
+        super().__init__()
+
+        self._connection = connection
+
+    async def send(
+        self, attr: AttrW[NumberT, WpiMicro4ControllerSettingIORef], value: NumberT
+    ) -> None:
+        command = f"{attr.io_ref.name}{attr.dtype(value)};"
+        await self._connection.send_command(f"{command}")
+
+
+# queries only
 @dataclass
 class WpiMicro4ControllerAttributeIORef(AttributeIORef):
     name: str
@@ -21,6 +46,7 @@ class WpiMicro4ControllerAttributeIORef(AttributeIORef):
     update_period: float | None = 0.5
 
 
+# Queries only
 class WpiMicro4ControllerAttributeIO(
     AttributeIO[NumberT, WpiMicro4ControllerAttributeIORef]
 ):
@@ -32,79 +58,52 @@ class WpiMicro4ControllerAttributeIO(
     async def update(self, attr: AttrR[NumberT, WpiMicro4ControllerAttributeIORef]):
         query = f"?{attr.io_ref.name}"
         response = await self._connection.send_query(f"{query}\r\n")
-        value = response.strip(query + " ; \r\n")
+        # make sure the query and response match
+        if query in response and ";" not in response:
+            value = response.strip(query + " \r\n")
+            await attr.update(attr.dtype(value))
+        # else: #what if it doesn't - new line arrives
 
-        if value != "":
-            if value != "Set line number:    L1...L4":
-                await attr.update(attr.dtype(value))
-
-    async def send(
-        self, attr: AttrW[NumberT, WpiMicro4ControllerAttributeIORef], value: NumberT
-    ) -> None:
-        command = f"{attr.io_ref.name}{attr.dtype(value)};"
-        await self._connection.send_command(f"{command}")
-
-
-@dataclass
-class WpiMicro4ControllerLineIORef(AttributeIORef):
-    name: str
-    _: KW_ONLY
-    update_period: float | None = 0.5
-
-
-class WpiMicro4ControllerLineIO(AttributeIO[NumberT, WpiMicro4ControllerLineIORef]):
-    def __init__(self, connection: IPConnection):
-        super().__init__()
-
-        self._connection = connection
-
-    async def update(self, attr: AttrR[NumberT, WpiMicro4ControllerLineIORef]):
-        query = f"?{attr.io_ref.name}"
-        response = await self._connection.send_query(f"{query}\r\n")
-        value = response.strip(query + " ; \r\n")
-
-        if value in ["11", "22", "33", "44"]:
-            line_number = attr.dtype(value)
-            last_digit = line_number % 10
-            await attr.update(last_digit)
-        elif value != "":
-            if value != "Set line number:    L1...L4":
-                await attr.update(attr.dtype(value))
-
-    async def send(
-        self, attr: AttrW[NumberT, WpiMicro4ControllerLineIORef], value: NumberT
-    ) -> None:
-        command = f"{attr.io_ref.name}{attr.dtype(value)};"
-        await self._connection.send_command(f"{command}")
+    # volume_counter = AttrW(Float(), io_ref=WpiMicro4ControllerAttributeIORef("C"))
+    # volume = AttrW(Float(), io_ref=WpiMicro4ControllerAttributeIORef("V"))
+    # delivery_rate = AttrW(Float(), io_ref=WpiMicro4ControllerAttributeIORef("R"))
+    # infuse_mode = AttrW(Bool(), io_ref=WpiMicro4ControllerAttributeIORef("I")) #on/off
+    # withdraw_mode =
+    # AttrW(Bool(), io_ref=WpiMicro4ControllerAttributeIORef("W")) #on/off
+    # go = AttrW(Bool(), io_ref=WpiMicro4ControllerAttributeIORef("G")) on/off
+    # halt = AttrW(Bool(), io_ref=WpiMicro4ControllerAttributeIORef("H")) #on/off
+    # rate_per_sec = AttrW(Bool(), io_ref=WpiMicro4ControllerAttributeIORef("S")) on/off
+    # rate_per_min = AttrW(Bool(), io_ref=WpiMicro4ControllerAttributeIORef("M")) on/off
+    # not_grupped = AttrW(Bool(), io_ref=WpiMicro4ControllerAttributeIORef("N")) on/off
+    # grupped = AttrW(Bool(), io_ref=WpiMicro4ControllerAttributeIORef("P")) on/off
+    # disabled = AttrW(Bool(), io_ref=WpiMicro4ControllerAttributeIORef("D")) on/off
 
 
 class WpiMicro4Controller(Controller):
-    line_number = AttrRW(Int(), io_ref=WpiMicro4ControllerLineIORef("L"))
-    volume_counter = AttrRW(Float(), io_ref=WpiMicro4ControllerAttributeIORef("C"))
-    volume = AttrRW(Float(), io_ref=WpiMicro4ControllerAttributeIORef("V"))
-    # volume_rate_rbv = AttrR(Float(),
-    #  io_ref=WpiMicro4ControllerAttributeIORef("R"))
-    # volume_max_rate_rbv = AttrR(Float(),
-    # io_ref=WpiMicro4ControllerAttributeIORef("X"))
+    line_number = AttrW(Int(), io_ref=WpiMicro4ControllerSettingIORef("L"))
+    volume_counter_1 = AttrW(
+        Float(), io_ref=WpiMicro4ControllerSettingIORef("C")
+    )  # TODO: check what happens to the value
+    volume_rbv = AttrR(Float(), io_ref=WpiMicro4ControllerAttributeIORef("V"))
+    # volume_counter_rbv = AttrR(Float(), io_ref=WpiMicro4ControllerAttributeIORef("C"))
+    # delivery_rate_rbv = AttrR(Float(), io_ref=WpiMicro4ControllerAttributeIORef("R"))
+    # volume_max_rate_rbv =
+    # AttrR(Float(), io_ref=WpiMicro4ControllerAttributeIORef("X"))
     # step_rate_rbv = AttrR(Float(), io_ref=WpiMicro4ControllerAttributeIORef("P"))
     # number_of_steps_rbv = AttrR(Float(),
     # io_ref=WpiMicro4ControllerAttributeIORef("T"))
-    beeper_on1_rbv = AttrR(String(), io_ref=WpiMicro4ControllerAttributeIORef("1"))
-    beeper_on2_rbv = AttrR(String(), io_ref=WpiMicro4ControllerAttributeIORef("2"))
+    # beeper_on1_rbv = AttrR(String(), io_ref=WpiMicro4ControllerAttributeIORef("1"))
+    # beeper_on2_rbv = AttrR(String(), io_ref=WpiMicro4ControllerAttributeIORef("2"))
     # press_once_to_run1_rbv = AttrR(
-    #    String(), io_ref=WpiMicro4ControllerAttributeIORef("3")
-    # )
+    #    String(), io_ref=WpiMicro4ControllerAttributeIORef("3"))
     # press_once_to_run2_rbv = AttrR(
-    #    String(), io_ref=WpiMicro4ControllerAttributeIORef("4")
-    # )
+    #    String(), io_ref=WpiMicro4ControllerAttributeIORef("4"))
     # microstepping_on1_rbv = AttrR(
-    #    String(), io_ref=WpiMicro4ControllerAttributeIORef("6")
-    # )
+    #    String(), io_ref=WpiMicro4ControllerAttributeIORef("6"))
     # microstepping_on2_rbv = AttrR(
-    #    String(), io_ref=WpiMicro4ControllerAttributeIORef("7")
-    # )
-    # grupped_mode_on_rbv = AttrR(String(),
-    # io_ref=WpiMicro4ControllerAttributeIORef("M"))
+    #    String(), io_ref=WpiMicro4ControllerAttributeIORef("7"))
+    # grupped_mode_on_rbv =
+    # AttrR(String(), io_ref=WpiMicro4ControllerAttributeIORef("M"))
     # pump_direction_rbv = AttrR(String(),
     # io_ref=WpiMicro4ControllerAttributeIORef("D"))
     # pump_units_rbv = AttrR(String(), io_ref=WpiMicro4ControllerAttributeIORef("U"))
@@ -118,9 +117,29 @@ class WpiMicro4Controller(Controller):
         super().__init__(
             ios=[
                 WpiMicro4ControllerAttributeIO(self.connection),
-                WpiMicro4ControllerLineIO(self.connection),
+                WpiMicro4ControllerSettingIO(self.connection),
             ]
         )
 
     async def connect(self):
         await self.connection.connect(self._ip_settings)
+
+    @command()
+    async def set_linen_nr1(self):
+        await self.line_number.put(self.line_number.dtype(1), sync_setpoint=True)
+        await asyncio.sleep(0.1)
+
+    @command()
+    async def set_linen_nr2(self):
+        await self.line_number.put(self.line_number.dtype(2), sync_setpoint=True)
+        await asyncio.sleep(0.11)
+
+    @command()
+    async def set_linen_nr3(self):
+        await self.line_number.put(self.line_number.dtype(3), sync_setpoint=True)
+        await asyncio.sleep(0.1)
+
+    @command()
+    async def set_linen_nr4(self):
+        await self.line_number.put(self.line_number.dtype(4), sync_setpoint=True)
+        await asyncio.sleep(0.1)
